19d4f953e810cbf00465c09a3d6dd1715f02f700
Frederikam <frogkr@gmail.com>
2016-03-16 09:20:50 +0100

    Command parser will now take quoted text ("") as a single argument

--- src/main/java/fredboat/commandmeta/CommandManager.java
+++ src/main/java/fredboat/commandmeta/CommandManager.java
@@ -2,6 +2,7 @@ package fredboat.commandmeta;
 
 import fredboat.FredBoat;
 import fredboat.util.TextUtils;
+import java.util.ArrayList;
 import java.util.HashMap;
 import net.dv8tion.jda.MessageBuilder;
 import net.dv8tion.jda.entities.Guild;
@@ -16,48 +17,95 @@ public class CommandManager {
     public static int commandsExecuted = 0;
 
     public static void registerCommand(String name, ICommand cmd) {
-        System.out.println("Registered new command " + name );
+        System.out.println("Registered new command " + name);
         commands.put(name, cmd);
     }
-    
+
     public static void registerAlias(String aliasName, String cmdName) {
         System.out.println("Registered new alias " + aliasName + " for " + cmdName);
         commands.put(aliasName, commands.get(cmdName));
     }
 
     public static void prefixCalled(Guild guild, TextChannel channel, User invoker, Message message) {
-        String[] args = message.getRawContent().replace("\n", " ").split(" ");
+        //String[] args = message.getRawContent().replace("\n", " ").split(" ");
+        String[] args = commandToArguments(message.getRawContent());
         ICommand invoked = commands.getOrDefault(args[0].substring(fredboat.FredBoat.PREFIX.length()), defaultCmd);
         commandsExecuted++;
         if (invoked instanceof ICommandOwnerRestricted) {
             //This command is restricted to only Frederikam
             //Check if invoker is actually Frederikam
-            if(!invoker.getId().equals(FredBoat.OWNER_ID)){
+            if (!invoker.getId().equals(FredBoat.OWNER_ID)) {
                 channel.sendMessage(TextUtils.prefaceWithMention(invoker, " you are not allowed to use that command!"));
                 return;
             }
         }
         try {
-            invoked.onInvoke(guild, channel, invoker, message, message.getContent().split(" "));
+            invoked.onInvoke(guild, channel, invoker, message, args);
         } catch (Exception e) {
             MessageBuilder builder = new MessageBuilder();
-            
+
             builder.appendMention(invoker);
-            builder.appendString(" an error occured :anger: ```java\n"+e.toString()+"\n");
-            
+            builder.appendString(" an error occured :anger: ```java\n" + e.toString() + "\n");
+
             //builder.appendString("```java\n");
-            for(StackTraceElement ste : e.getStackTrace()){
-                builder.appendString("\t"+ste.toString()+"\n");
-                if("prefixCalled".equals(ste.getMethodName())){
+            for (StackTraceElement ste : e.getStackTrace()) {
+                builder.appendString("\t" + ste.toString() + "\n");
+                if ("prefixCalled".equals(ste.getMethodName())) {
                     break;
                 }
             }
             builder.appendString("\t...```");
-            
+
             channel.sendMessage(builder.build());
         }
-        
 
     }
 
+    public static String[] commandToArguments(String cmd) {
+        ArrayList<String> a = new ArrayList<>();
+        int argi = 0;
+        boolean isInQuote = false;
+
+        for (Character ch : cmd.toCharArray()) {
+            if (Character.isWhitespace(ch) && isInQuote == false) {
+                String arg = null;
+                try {
+                    arg = a.get(argi);
+                } catch (IndexOutOfBoundsException e) {
+                }
+                if (arg != null) {
+                    argi++;//On to the next arg
+                }//else ignore
+
+            } else if (ch.equals('"')) {
+                isInQuote = !isInQuote;
+            } else {
+                a = writeToArg(a, argi, ch);
+            }
+        }
+
+        String[] newA = new String[a.size()];
+        int i = 0;
+        for (String str : a) {
+            newA[i] = str;
+            i++;
+        }
+
+        return newA;
+    }
+
+    private static ArrayList<String> writeToArg(ArrayList<String> a, int argi, char ch) {
+        String arg = null;
+        try {
+            arg = a.get(argi);
+        } catch (IndexOutOfBoundsException e) {
+        }
+        if (arg == null) {
+            a.add(argi, String.valueOf(ch));
+        } else {
+            a.set(argi, arg + ch);
+        }
+        
+        return a;
+    }
 }
