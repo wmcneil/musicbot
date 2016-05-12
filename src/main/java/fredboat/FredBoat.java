e5bba60eabcb4cfc079f448c17cd4d12ed0bbf5a
Frederikam <frogkr@gmail.com>
2016-05-12 23:03:23 +0200

    This pun command is so bad it doesn't even hurt.

--- src/main/java/fredboat/FredBoat.java
+++ src/main/java/fredboat/FredBoat.java
@@ -37,7 +37,6 @@ import net.dv8tion.jda.JDAInfo;
 import net.dv8tion.jda.client.JDAClientBuilder;
 import net.dv8tion.jda.entities.User;
 import org.json.JSONObject;
-import redis.clients.jedis.Client;
 import redis.clients.jedis.Jedis;
 
 public class FredBoat {
@@ -181,8 +180,13 @@ public class FredBoat {
         CommandRegistry.registerCommand(0x11, "fuck", new RemoteFileCommand("http://i.imgur.com/1bllKNh.png"));
         CommandRegistry.registerCommand(0x11, "idc", new RemoteFileCommand("http://i.imgur.com/0ZPjpNg.png"));
         CommandRegistry.registerCommand(0x11, "beingraped", new RemoteFileCommand("http://i.imgur.com/dPsYRYV.png"));
+        CommandRegistry.registerCommand(0x11, "anime", new RemoteFileCommand("https://cdn.discordapp.com/attachments/132490115137142784/177751190333816834/animeeee.png"));
+        CommandRegistry.registerCommand(0x11, "wow", new RemoteFileCommand("http://img.prntscr.com/img?url=http://i.imgur.com/aexiMAG.png"));
+        CommandRegistry.registerCommand(0x11, "what", new RemoteFileCommand("http://i.imgur.com/CTLraK4.png"));
+        CommandRegistry.registerCommand(0x11, "pun", new RemoteFileCommand("http://i.imgur.com/2hFMrjt.png"));
         CommandRegistry.registerCommand(0x11, "suicide", new RemoteFileCommand("http://nekomata.moe/i/S3g6D0.png"));
         CommandRegistry.registerCommand(0x11, "die", new RemoteFileCommand("http://nekomata.moe/i/k2B0f6.png"));
         CommandRegistry.registerCommand(0x11, "stupid", new RemoteFileCommand("http://nekomata.moe/i/c056y7.png"));
+        
     }
 }
