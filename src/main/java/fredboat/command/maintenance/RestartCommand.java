db2af7f1529c8a20a9068bbdf771b31d6d12c04e
Frederikam <frogkr@gmail.com>
2016-05-18 12:47:40 +0200

    Changed the restart command to use the restart exit code

--- src/main/java/fredboat/command/maintenance/RestartCommand.java
+++ src/main/java/fredboat/command/maintenance/RestartCommand.java
@@ -1,7 +1,9 @@
 package fredboat.command.maintenance;
 
+import fredboat.FredBoat;
 import fredboat.commandmeta.Command;
 import fredboat.commandmeta.ICommandOwnerRestricted;
+import fredboat.util.ExitCodes;
 import fredboat.util.TextUtils;
 import net.dv8tion.jda.entities.Guild;
 import net.dv8tion.jda.entities.Message;
@@ -12,38 +14,13 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 
-/**
- *
- * @author frederik
- */
 public class RestartCommand extends Command implements ICommandOwnerRestricted {
 
     @Override
     public void onInvoke(Guild guild, TextChannel channel, User invoker, Message message, String[] args) {
         channel.sendMessage(TextUtils.prefaceWithMention(invoker, " Restarting.."));
-        try {
-            final String javaBin = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
-            final File currentJar = new File(RestartCommand.class.getProtectionDomain().getCodeSource().getLocation().toURI());
-            
-            /* is it a jar file? */
-            if (!currentJar.getName().endsWith(".jar")) {
-                throw new RuntimeException("Cannot restart in an IDE environment!");
-            }
-            
-            /* Build command: java -jar application.jar */
-            final ArrayList<String> command = new ArrayList<>();
-            command.add(javaBin);
-            command.add("-jar");
-            command.add(currentJar.getPath());
-            
-            final ProcessBuilder builder = new ProcessBuilder(command);
-            builder.start();
-            channel.sendMessage(TextUtils.prefaceWithMention(invoker, " New process started, ending this proces."));
-            System.exit(0);
-        } catch (IOException | URISyntaxException ex) {
-            ex.printStackTrace(System.out);
-            throw new RuntimeException(ex);
-        }
+        
+        FredBoat.shutdown(ExitCodes.EXIT_CODE_RESTART);
     }
 
 }
