2c6165764a32600983f9a867ccf982a84e0ed820
Frederikam <frogkr@gmail.com>
2016-06-21 19:56:42 +0200

    Updated Sergi memes

--- src/main/java/fredboat/FredBoat.java
+++ src/main/java/fredboat/FredBoat.java
@@ -204,7 +204,7 @@ public class FredBoat {
         CommandRegistry.registerCommand(0x11, "mal", new MALCommand());
 
         //Begin sergi memes//
-        CommandRegistry.registerCommand(0x11, "welcome", new RemoteFileCommand("https://cdn.discordapp.com/attachments/132490115137142784/176587676093251585/2f16d4b6d0.png"));
+        CommandRegistry.registerCommand(0x11, "welcome", new RemoteFileCommand("http://i.imgur.com/9g6SdtH.png"));
         CommandRegistry.registerCommand(0x11, "rude", new RemoteFileCommand("http://i.imgur.com/pUn7ijx.png"));
         CommandRegistry.registerCommand(0x11, "fuck", new RemoteFileCommand("http://i.imgur.com/1bllKNh.png"));
         CommandRegistry.registerCommand(0x11, "idc", new RemoteFileCommand("http://i.imgur.com/0ZPjpNg.png"));
@@ -213,7 +213,6 @@ public class FredBoat {
         CommandRegistry.registerCommand(0x11, "wow", new RemoteFileCommand("http://img.prntscr.com/img?url=http://i.imgur.com/aexiMAG.png"));
         CommandRegistry.registerCommand(0x11, "what", new RemoteFileCommand("http://i.imgur.com/CTLraK4.png"));
         CommandRegistry.registerCommand(0x11, "pun", new RemoteFileCommand("http://i.imgur.com/2hFMrjt.png"));
-        CommandRegistry.registerCommand(0x11, "suicide", new RemoteFileCommand("http://nekomata.moe/i/S3g6D0.png"));
         CommandRegistry.registerCommand(0x11, "die", new RemoteFileCommand("http://nekomata.moe/i/k2B0f6.png"));
         CommandRegistry.registerCommand(0x11, "stupid", new RemoteFileCommand("http://nekomata.moe/i/c056y7.png"));
         CommandRegistry.registerCommand(0x11, "stop", new RemoteFileCommand("http://puu.sh/oQN2j/8e09872842.jpg"));
