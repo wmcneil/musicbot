84f5bc9c48cffcc619a822b5ad2cdee1988ff397
Frederikam <frogkr@gmail.com>
2016-08-21 01:21:29 +0200

    Improved exit command

--- src/main/java/fredboat/command/maintenance/ExitCommand.java
+++ src/main/java/fredboat/command/maintenance/ExitCommand.java
@@ -3,6 +3,7 @@ package fredboat.command.maintenance;
 import fredboat.FredBoat;
 import fredboat.commandmeta.Command;
 import fredboat.commandmeta.ICommandOwnerRestricted;
+import fredboat.util.ExitCodes;
 import net.dv8tion.jda.entities.Guild;
 import net.dv8tion.jda.entities.Message;
 import net.dv8tion.jda.entities.TextChannel;
@@ -19,7 +20,7 @@ public class ExitCommand extends Command implements ICommandOwnerRestricted {
     public void onInvoke(Guild guild, TextChannel channel, User invoker, Message message, String[] args) {
         if (invoker.getId().equals(FredBoat.OWNER_ID)) {
             channel.sendMessage(TextUtils.prefaceWithMention(invoker, " goodbye!!"));
-            System.exit(0);
+            FredBoat.shutdown(ExitCodes.EXIT_CODE_NORMAL);
         } else {
             channel.sendMessage(TextUtils.prefaceWithMention(invoker, " you are not allowed to use that command!"));
         }
