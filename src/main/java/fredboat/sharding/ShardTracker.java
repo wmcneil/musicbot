1179f509a616f5edf75eb1ca1b430b4f6546d937
Frederik Ar. Mikkelsen <frogkr@gmail.com>
2016-09-23 12:38:36 +0200

    Added shard tracker loop

--- src/main/java/fredboat/sharding/ShardTracker.java
+++ src/main/java/fredboat/sharding/ShardTracker.java
@@ -1,38 +1,54 @@
 package fredboat.sharding;
 
 import net.dv8tion.jda.JDA;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ShardTracker extends Thread {
-    
+
+    private static final Logger log = LoggerFactory.getLogger(ShardTracker.class);
     public final JDA jda;
     public static ShardTracker ins = null;
-    
+
     private static int globalUserCount = 0;
     private static int globalGuildCount = 0;
-    
+
+    private static final long TICK_TIME = 30 * 60 * 1000;//30 mins
+
     public ShardTracker(JDA jda, String token) {
-        if(ins != null){
+        if (ins != null) {
             throw new IllegalStateException("Only one instance may exist.");
         }
-        
+
         this.jda = jda;
-        
+
         setDaemon(true);
         setName(this.getClass().getSimpleName());
         ins = this;
     }
 
-    public static int getGlobalUserCount(){
+    public static int getGlobalUserCount() {
         return globalUserCount;
     }
-    
-    public static int getGlobalGuildCount(){
+
+    public static int getGlobalGuildCount() {
         return globalGuildCount;
     }
-    
+
     @Override
     public void run() {
-        
+        while (true) {
+            try {
+                sleep(TICK_TIME);
+                tick();
+            } catch (Exception ex) {
+                log.error("Caught exception in ShardTracker loop", ex);
+            }
+        }
     }
-    
+
+    private void tick() {
+
+    }
+
 }
