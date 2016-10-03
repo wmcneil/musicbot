6fd4c17ca3012a9fec26ff899f9dfe6eadfbff2d
Frederik Ar. Mikkelsen <frogkr@gmail.com>
2016-10-03 12:32:15 +0200

    Removed old UptimeRobot data

--- src/main/java/fredboat/command/maintenance/StatsCommand.java
+++ src/main/java/fredboat/command/maintenance/StatsCommand.java
@@ -1,7 +1,5 @@
 package fredboat.command.maintenance;
 
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
 import fredboat.FredBoat;
 import fredboat.audio.PlayerRegistry;
 import fredboat.commandmeta.CommandManager;
@@ -15,8 +13,6 @@ import net.dv8tion.jda.entities.Guild;
 import net.dv8tion.jda.entities.Message;
 import net.dv8tion.jda.entities.TextChannel;
 import net.dv8tion.jda.entities.User;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class StatsCommand extends Command {
 
@@ -27,17 +23,6 @@ public class StatsCommand extends Command {
         int hours = (int) ((totalSecs / (60 * 60)) % 24);
         int mins = (int) ((totalSecs / 60) % 60);
         int secs = (int) (totalSecs % 60);
-
-        String uptime30Days = "ERROR";
-        String url = "https://api.uptimerobot.com/getMonitors?apiKey=m777550344-171dca574df5a9f8dc7f2484&responseTimesAverage=180&monitors=15830-32696&customUptimeRatio=30&format=json";
-        try {
-            String jsonstr = Unirest.get(url).asString().getBody().substring("jsonUptimeRobotApi(".length());
-            jsonstr = jsonstr.substring(0, jsonstr.length() - 1);
-            uptime30Days = new JSONObject(jsonstr).getJSONObject("monitors").getJSONArray("monitor").getJSONObject(0).getString("customuptimeratio") + "%";
-            //uptime30Days = Unirest.get(url).asJson().getBody().getObject().getJSONObject("monitors").getJSONArray("monitor").getJSONObject(0).getString("customUptimeRatio") + "%";
-        } catch (JSONException | UnirestException ex) {
-            ex.printStackTrace();
-        }
         
         String str = " This shard has been running for "
                 + days + " days, "
@@ -51,7 +36,6 @@ public class StatsCommand extends Command {
         }
         
         str = str + "That's a rate of " + (float) (CommandManager.commandsExecuted - 1) / ((float) totalSecs / (float) (60 * 60)) + " commands per hour\n\n```";
-        str = str + "Host uptime last 30 days:  " + uptime30Days + "\n";
         str = str + "Reserved memory:           " + Runtime.getRuntime().totalMemory() / 1000000 + "MB\n";
         str = str + "-> Of which is used:       " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1000000 + "MB\n";
         str = str + "-> Of which is free:       " + Runtime.getRuntime().freeMemory() / 1000000 + "MB\n";
