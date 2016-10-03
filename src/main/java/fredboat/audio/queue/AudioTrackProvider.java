2516a08e7d93b7ec7615596ec3a15393797b0513
Frederik Ar. Mikkelsen <frogkr@gmail.com>
2016-10-03 18:53:01 +0200

    Create AudioTrackProvider

--- src/main/java/fredboat/audio/queue/AudioTrackProvider.java
+++ src/main/java/fredboat/audio/queue/AudioTrackProvider.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fredboat.audio.queue;
 
-/**
- *
- * @author frederik
- */
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+
 public interface AudioTrackProvider {
+    
+    public AudioTrack provideAudioTrack();
     
 }
