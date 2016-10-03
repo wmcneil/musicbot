1bfe9c45327ce89a50c48f76c6110cb41092f56a
Frederik Ar. Mikkelsen <frogkr@gmail.com>
2016-10-03 19:27:36 +0200

    Added more essential methods to the abstract player

--- src/main/java/fredboat/audio/AbstractPlayer.java
+++ src/main/java/fredboat/audio/AbstractPlayer.java
@@ -7,8 +7,8 @@ import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import fredboat.audio.queue.AudioTrackProvider;
 import net.dv8tion.jda.audio.AudioSendHandler;
 
-public abstract class AbstractPlayer extends AudioEventAdapter implements AudioSendHandler{
-    
+public abstract class AbstractPlayer extends AudioEventAdapter implements AudioSendHandler {
+
     private static AudioPlayerManager playerManager;
     private AudioPlayer player;
     private AudioTrackProvider audioTrackProvider;
@@ -17,30 +17,46 @@ public abstract class AbstractPlayer extends AudioEventAdapter implements AudioS
     protected AbstractPlayer() {
         initAudioPlayerManager();
         player = new AudioPlayer(playerManager);
-        
+
         player.addListener(this);
     }
-    
-    private static void initAudioPlayerManager(){
+
+    private static void initAudioPlayerManager() {
         playerManager = new AudioPlayerManager();
     }
-    
+
     public void play() {
-        if(player.isPaused()){
+        if (player.isPaused()) {
             player.setPaused(false);
         }
     }
-    
+
     public void pause() {
         player.setPaused(true);
     }
-    
+
     public void skip() {
         player.stopTrack();
     }
-    
-    public void setVolume(float vol){
+
+    public boolean isPlaying() {
+        return player.getPlayingTrack() != null && !player.isPaused();
+    }
+
+    public long getCurrentTimestamp() {
+        return player.getPlayingTrack().getPosition();
+    }
+
+    public AudioTrack getPlayingTrack() {
+        return player.getPlayingTrack();
+    }
+
+    public void setVolume(float vol) {
         throw new UnsupportedOperationException("AudioPlayer does not yet support volume control");
+    }
+
+    public float getVolume() {
+        return -1f;//Not yet supported
     }
 
     public void setAudioTrackProvider(AudioTrackProvider audioTrackProvider) {
@@ -49,19 +65,24 @@ public abstract class AbstractPlayer extends AudioEventAdapter implements AudioS
 
     @Override
     public void onTrackEnd(AudioPlayer player, AudioTrack track, boolean interrupted) {
-        if(audioTrackProvider != null){
+        if (audioTrackProvider != null) {
             player.playTrack(audioTrackProvider.provideAudioTrack());
         }
     }
 
     @Override
     public boolean canProvide() {
-        return player.getPlayingTrack() != null && !player.isPaused();
+        return isPlaying();
     }
 
     @Override
     public byte[] provide20MsAudio() {
         return player.provide().data;
     }
-    
+
+    @Override
+    public boolean isOpus() {
+        return true;
+    }
+
 }
