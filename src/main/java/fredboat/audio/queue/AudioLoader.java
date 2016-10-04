95fd534686f01a3091faed7b3ad623b84ff8534e
Frederik Ar. Mikkelsen <frogkr@gmail.com>
2016-10-04 15:27:59 +0200

    Created AudioLoader

---
+++ src/main/java/fredboat/audio/queue/AudioLoader.java
@@ -0,0 +1,71 @@
+package fredboat.audio.queue;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class AudioLoader implements AudioLoadResultHandler {
+
+    private final AbstractTrackProvider trackProvider;
+    private final AudioPlayerManager playerManager;
+    private final ConcurrentLinkedQueue<IdentifierContext> identifierQueue = new ConcurrentLinkedQueue();
+    private IdentifierContext contextBeingLoaded = null;
+    private volatile boolean isLoading = false;
+
+    public AudioLoader(AbstractTrackProvider trackProvider, AudioPlayerManager playerManager) {
+        this.trackProvider = trackProvider;
+        this.playerManager = playerManager;
+    }
+    
+    public void loadAsync(IdentifierContext ic){
+        identifierQueue.add(ic);
+        if(!isLoading){
+            loadNextAsync();
+        }
+    }
+    
+    private void loadNextAsync(){
+        IdentifierContext ic = identifierQueue.poll();
+        if(ic != null){
+            isLoading = true;
+            contextBeingLoaded = ic;
+            playerManager.loadItem(ic.identifier, this);
+        } else {
+            isLoading = false;
+        }
+    }
+    
+    @Override
+    public void trackLoaded(AudioTrack at) {
+        trackProvider.add(at);
+        loadNextAsync();
+    }
+
+    @Override
+    public void playlistLoaded(AudioPlaylist ap) {
+        for(AudioTrack at : ap.getTracks()){
+            trackProvider.add(at);
+        }
+        loadNextAsync();
+    }
+
+    @Override
+    public void noMatches() {
+        contextBeingLoaded.textChannel.sendMessage(
+                "No audio could be found for `" + contextBeingLoaded.identifier + "`."
+        );
+        loadNextAsync();
+    }
+
+    @Override
+    public void loadFailed(FriendlyException fe) {
+        contextBeingLoaded.textChannel.sendMessage(
+                "Error when loading info for `" + contextBeingLoaded.identifier + "`: " + fe.getMessage()
+        );
+        loadNextAsync();
+    }
+    
+}
