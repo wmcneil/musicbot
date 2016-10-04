583217dc4a84800e4f3c542e7cbb6c4233852e0d
Frederik Ar. Mikkelsen <frogkr@gmail.com>
2016-10-04 21:59:30 +0200

    Fix ;;list

--- src/main/java/fredboat/command/music/ListCommand.java
+++ src/main/java/fredboat/command/music/ListCommand.java
@@ -1,5 +1,6 @@
 package fredboat.command.music;
 
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import fredboat.audio.GuildPlayer;
 import fredboat.audio.PlayerRegistry;
 import fredboat.commandmeta.abs.Command;
@@ -9,7 +10,6 @@ import net.dv8tion.jda.entities.Guild;
 import net.dv8tion.jda.entities.Message;
 import net.dv8tion.jda.entities.TextChannel;
 import net.dv8tion.jda.entities.User;
-import net.dv8tion.jda.player.source.AudioSource;
 
 public class ListCommand extends Command implements IMusicCommand {
 
@@ -17,44 +17,41 @@ public class ListCommand extends Command implements IMusicCommand {
     public void onInvoke(Guild guild, TextChannel channel, User invoker, Message message, String[] args) {
         GuildPlayer player = PlayerRegistry.get(guild.getId());
         player.currentTC = channel;
-        if (player.getCurrentAudioSource() != null || !player.getAudioQueue().isEmpty()) {
+        if (player.getPlayingTrack() != null) {
             MessageBuilder mb = new MessageBuilder();
-            if (player.getCurrentAudioSource() != null) {
-                String status = player.isPlaying() ? "[PLAYING] " : "[PAUSED] ";
-                mb.appendString(status, MessageBuilder.Formatting.BOLD)
-                        .appendString(player.getCurrentAudioSource().getInfo().getTitle())
-                        .appendString("\n");
-            }
-
-            //Now add the queue
             int i = 1;
-            for (AudioSource src : player.getAudioQueue()) {
-                mb.appendString(src.getInfo().getTitle())
-                        .appendString("\n");
-                if (i == 10) {
-                    break;
+            for (AudioTrack at : player.getRemainingTracks()) {
+                if (i == 0) {
+                    String status = player.isPlaying() ? "[PLAYING] " : "[PAUSED] ";
+                    mb.appendString(status, MessageBuilder.Formatting.BOLD)
+                            .appendString(at.getInfo().title)
+                            .appendString("\n");
+                } else {
+                    mb.appendString(at.getInfo().title)
+                            .appendString("\n");
+                    if (i == 10) {
+                        break;
+                    }
                 }
                 i++;
             }
-
-            int add = player.getCurrentAudioSource() != null ? 1 : 0;
 
             //Now add a timestamp for how much is remaining
             int t = player.getTotalRemainingMusicTimeSeconds();
             int sec = t % 60;
             int min = (t % 3600) / 60;
             int hrs = t / 3600;
-            
+
             String timestamp;
-            
-            if (hrs != 0){
-                timestamp = forceTwoDigits(hrs) +":"+ forceTwoDigits(min) +":"+ forceTwoDigits(sec);
+
+            if (hrs != 0) {
+                timestamp = forceTwoDigits(hrs) + ":" + forceTwoDigits(min) + ":" + forceTwoDigits(sec);
             } else {
-                timestamp = forceTwoDigits(min) +":"+ forceTwoDigits(sec);
+                timestamp = forceTwoDigits(min) + ":" + forceTwoDigits(sec);
             }
 
             mb.appendString("\n\nThere are a total of **")
-                    .appendString(String.valueOf(player.getAudioQueue().size() + add), MessageBuilder.Formatting.BOLD)
+                    .appendString(String.valueOf(player.getRemainingTracks().size()), MessageBuilder.Formatting.BOLD)
                     .appendString("** queued songs with a remaining length of **" + timestamp + "**.");
 
             channel.sendMessage(mb.build());
