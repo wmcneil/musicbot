2179b670d1a05bacc7f8ca4bacf90a0bfc2acd4d
Frederik Ar. Mikkelsen <frogkr@gmail.com>
2016-10-08 14:03:41 +0200

    Fixed NPE in ;;join command

--- src/main/java/fredboat/command/music/JoinCommand.java
+++ src/main/java/fredboat/command/music/JoinCommand.java
@@ -8,19 +8,22 @@ import net.dv8tion.jda.entities.Guild;
 import net.dv8tion.jda.entities.Message;
 import net.dv8tion.jda.entities.TextChannel;
 import net.dv8tion.jda.entities.User;
+import net.dv8tion.jda.entities.VoiceChannel;
 
 public class JoinCommand extends Command implements IMusicCommand {
 
     @Override
     public void onInvoke(Guild guild, TextChannel channel, User invoker, Message message, String[] args) {
         GuildPlayer player = PlayerRegistry.get(guild.getId());
+        VoiceChannel vc = player.getUserCurrentVoiceChannel(invoker);
         player.setCurrentTC(channel);
         try {
-            player.joinChannel(invoker);
-            channel.sendMessage("Joining " + player.getChannel().getName());
+            player.joinChannel(vc);
+            if (vc != null) {
+                channel.sendMessage("Joining " + vc.getName());
+            }
         } catch (IllegalStateException ex) {
             channel.sendMessage("An error occurred. Couldn't join " + player.getChannel().getName() + " because I am already trying to connect to that channel. Please try again.");
-            return;
         }
     }
 
