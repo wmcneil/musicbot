057a012b3b8e42bc229440d656e5371a47deddf6
Frederik Ar. Mikkelsen <frogkr@gmail.com>
2016-12-14 11:39:53 +0100

    Cleaned up DistributionEnum

--- FredBoat/src/main/java/fredboat/util/DistributionEnum.java
+++ FredBoat/src/main/java/fredboat/util/DistributionEnum.java
@@ -15,45 +15,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public enum DistributionEnum {
-    MAIN(3400, "production"), MUSIC(3500, "music"), BETA(3600, "beta"), PATRON(3700, "patron");
+    MAIN("production"),
+    MUSIC("music"),
+    BETA("beta"),
+    PATRON("patron");
 
-    private final int shard0Port;
     private final String id;
 
-    private DistributionEnum(int shard0Port, String id) {
-        this.shard0Port = shard0Port;
+    DistributionEnum(String id) {
         this.id = id;
-    }
-
-    public String getUrlForShard(int shardId) {
-        switch (this) {
-            case BETA:
-                return "http://localhost:" + (shard0Port + shardId) + "/";
-            case MAIN:
-                return "http://fb" + shardId + ".frederikam.com:" + getPort(shardId) + "/";
-            case MUSIC:
-                return "http://fbm" + shardId + ".frederikam.com:" + getPort(shardId) + "/";
-            default:
-                throw new IllegalArgumentException("Not a valid distribution type");
-        }
-    }
-
-    public List<String> getUrlsForOtherShard(int shardId, int numShards) {
-        ArrayList<String> list = new ArrayList<>();
-
-        for (int i = 0; i < numShards; i++) {
-            if (i == shardId) {
-                continue;
-            }
-
-            list.add(this.getUrlForShard(i));
-        }
-
-        return list;
-    }
-
-    public int getPort(int shardId) {
-        return this.shard0Port + shardId;
     }
 
     public String getId() {
