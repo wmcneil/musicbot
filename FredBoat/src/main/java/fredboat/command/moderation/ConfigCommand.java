29b0aa4c63726fd74161daeaba7606c446761e4b
Frederik Ar. Mikkelsen <frogkr@gmail.com>
2017-01-05 10:00:49 +0100

    Added autoplay option to config

--- FredBoat/src/main/java/fredboat/command/moderation/ConfigCommand.java
+++ FredBoat/src/main/java/fredboat/command/moderation/ConfigCommand.java
@@ -30,9 +30,9 @@ public class ConfigCommand extends Command {
 
         MessageBuilder mb = new MessageBuilder()
                 .append("Configuration for **" + guild.getName() + "**:```\n")
-                .append("track_announce = ")
-                .append(gc.isTrackAnnounce())
-                .append("\n```");
+                .append("track_announce = ").append(gc.isTrackAnnounce()).append("\n")
+                .append("autoplay = ").append(gc.isAutoplay()).append("\n")
+                .append("```");
 
         channel.sendMessage(mb.build()).queue();
     }
@@ -54,9 +54,17 @@ public class ConfigCommand extends Command {
         String val = args[2];
 
         if(key.equals("track_announce")) {
-            if(val.equalsIgnoreCase("true") | val.equalsIgnoreCase("false")) {
+            if (val.equalsIgnoreCase("true") | val.equalsIgnoreCase("false")) {
                 gc.setTrackAnnounce(Boolean.valueOf(val));
-                TextUtils.replyWithName(channel, invoker, "`track_announce` is now set to `"+val+"`.");
+                TextUtils.replyWithName(channel, invoker, "`track_announce` is now set to `" + val + "`.");
+                EntityWriter.mergeGuildConfig(gc);
+            } else {
+                channel.sendMessage(invoker.getEffectiveName() + ": This value must be true or false.").queue();
+            }
+        } else if(key.equals("authoplay")) {
+            if(val.equalsIgnoreCase("true") | val.equalsIgnoreCase("false")) {
+                gc.setAutoplay(Boolean.valueOf(val));
+                TextUtils.replyWithName(channel, invoker, "`authoplay` is now set to `"+val+"`.");
                 EntityWriter.mergeGuildConfig(gc);
             } else {
                 channel.sendMessage(invoker.getEffectiveName() + ": This value must be true or false.").queue();
