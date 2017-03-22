c85639543a1abda63320bc3aaa1a42676f6d6360
Frederik Ar. Mikkelsen <frogkr@gmail.com>
2017-03-22 11:31:17 +0100

    Did some cleanup in the config system

--- FredBoat/src/main/java/fredboat/Config.java
+++ FredBoat/src/main/java/fredboat/Config.java
@@ -34,10 +34,16 @@ import org.slf4j.LoggerFactory;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 public class Config {
 
@@ -144,6 +150,41 @@ public class Config {
         } catch (IOException | UnirestException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * Makes sure the requested config file exists in the current format. Will attempt to migrate old formats to new ones
+     * old files will be renamed to filename.ext.old to preserve any data
+     *
+     * @param name relative name of a config file, without the file extension
+     * @return a handle on the requested file
+     */
+    static File loadConfigFile(String name) throws IOException {
+        String yamlPath = "./" + name + ".yaml";
+        String jsonPath = "./" + name + ".json";
+        File yamlFile = new File(yamlPath);
+        if (!yamlFile.exists() || yamlFile.isDirectory()) {
+            log.warn("Could not find file '" + yamlPath + "', looking for legacy '" + jsonPath + "' to rewrite");
+            File json = new File(jsonPath);
+            if (!json.exists() || json.isDirectory()) {
+                //file is missing
+                log.error("No " + name + " file is present. Bot cannot run without it. Check the documentation.");
+                throw new FileNotFoundException("Neither '" + yamlPath + "' nor '" + jsonPath + "' present");
+            } else {
+                //rewrite the json to yaml
+                Yaml yaml = new Yaml();
+                String fileStr = FileUtils.readFileToString(json, "UTF-8");
+                //remove tab character from json file to make it a valid YAML file
+                fileStr = fileStr.replaceAll("\t", "");
+                @SuppressWarnings("unchecked")
+                Map<String, Object> configFile = (Map) yaml.load(fileStr);
+                yaml.dump(configFile, new FileWriter(yamlFile));
+                Files.move(Paths.get(jsonPath), Paths.get(jsonPath + ".old"), REPLACE_EXISTING);
+                log.info("Migrated file '" + jsonPath + "' to '" + yamlPath + "'");
+            }
+        }
+
+        return yamlFile;
     }
 
     public String getRandomGoogleKey() {
